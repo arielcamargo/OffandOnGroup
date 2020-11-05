a84ab5f35f0a47fbe36e15d17e7291b4696962b2
arielcamargo <70982774+arielcamargo@users.noreply.github.com>
2020-11-05 13:20:35 -0800

    Delete main.java

--- src/src/main.java
+++
@@ -1,9 +0,0 @@
-// This is how main looks like, everything minus the user line is essential for a program to run.
-public class main 
-{
-	public static void main(String args[])
-	{
-		//The line below creates a user object. Its kind of like calling a method, but it does a little more, so we have to go to User.java to get more info.
-		User x = new User();
-	}
-}
